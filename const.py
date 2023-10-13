3acd7dc8676441d8ae7b8d31faedd88ee0d2caf0
Boogeytroll <147802405+Boogeytroll@users.noreply.github.com>
2023-10-13 22:00:15 +0300

    Update const.py

---
+++ const.py
@@ -0,0 +1,7 @@
+"""Constants for the SwitchBot Cloud integration."""
+from datetime import timedelta
+from typing import Final
+
+DOMAIN: Final = "switchbot_cloud test"
+ENTRY_TITLE = "SwitchBot Cloud Test"
+SCAN_INTERVAL = timedelta(seconds=600)
